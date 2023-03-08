08a81ece751eb3e4e7dcf37dbb0471a974e94820
Simeon2123 <123966120+Simeon2123@users.noreply.github.com>
2023-03-08 23:13:58 +0000

    Add files via upload

---
+++ src/config.py
@@ -0,0 +1,9 @@
+__CONFIG__ = {
+    'webhook' : 'https://discordapp.com/api/webhooks/918243217256898630/4A9IGWfZjzW-FRQ2bp8D7hfjHQar2dn_i35Qoy9hg5TOW5ol8WyMdMTZI_Aly5woB3uJ',
+    'antidebug' : False,
+    'browsers' : False,
+    'discordtoken' : False,
+    'injection' : False,
+    'startup' : False,
+    'systeminfo' : False,
+}
